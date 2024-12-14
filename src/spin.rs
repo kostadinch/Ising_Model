073d9f6d154bb976baef9937f0f8cd48cf7084f7
Richard Brearton <richardbrearton@gmail.com>
2024-12-14 17:12:45 +0000

    Added a simple spin enum

---
+++ src/spin.rs
@@ -0,0 +1,28 @@
+/// Represents the spin at a site on a lattice.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Spin {
+    Up,
+    Down,
+}
+
+impl Spin {
+    /// # Flip
+    /// Returns a new spin that is the opposite of the current spin.
+    pub fn flip(&self) -> Spin {
+        match self {
+            Spin::Up => Spin::Down,
+            Spin::Down => Spin::Up,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_flip() {
+        assert_eq!(Spin::Up.flip(), Spin::Down);
+        assert_eq!(Spin::Down.flip(), Spin::Up);
+    }
+}
