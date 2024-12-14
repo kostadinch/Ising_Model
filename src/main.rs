5e940b2f5270e4d4d10b76d2b6cebfc471476f85
Richard Brearton <richardbrearton@gmail.com>
2024-12-14 18:34:13 +0000

    Added some final logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use grid::Grid;
 
 pub mod grid;
@@ -5,13 +7,16 @@ pub mod spin;
 
 fn main() {
     // Defining initial values.
-    let size_of_the_square_matrix = 50;
+    let size_of_the_square_matrix = 100;
     let coupling_between_neighboring_spins = 0.44;
     let applied_field = 0.02;
+    let number_of_sweeps = 7000;
 
+    // Create a new grid with random spins.
     let mut grid = Grid::new_random(size_of_the_square_matrix, size_of_the_square_matrix);
 
-    let number_of_sweeps = 7000;
+    // Start the timer
+    let start = Instant::now();
     for step in 0..number_of_sweeps {
         if step % 100 == 0 {
             println!("Sweep number: {}", step);
@@ -20,4 +25,5 @@ fn main() {
     }
 
     println!("Final configuration (sample element): {:?}", grid);
+    println!("Elapsed time: {:?}", start.elapsed());
 }
